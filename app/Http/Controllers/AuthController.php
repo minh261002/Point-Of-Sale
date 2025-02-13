efc4a92219ce17a27d3a5c9687eeb2e162c0f7ae
minhttran <trcongminh.261002@gmail.com>
2025-02-13 12:05:22 +0700

    implement login functionality with route protection and create login view

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
-class AuthController extends Controller
+class AuthController
 {
     public function login(): View
     {
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth:web'])->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('home');
+});
+
+Route::middleware(['login'])->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
 });
