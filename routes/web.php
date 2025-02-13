4cdeaa3d93649c0a1d361eec7316da0a36a68a61
minhttran <trcongminh.261002@gmail.com>
2025-02-13 12:25:06 +0700

    add password reset functionality and enhance layout with toggle columns

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,11 @@ Route::middleware(['auth:web'])->group(function () {
 
 Route::middleware(['login'])->group(function () {
     Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
+
+    Route::get('/password/forgot', [AuthController::class, 'forgotPassword'])->name('password.forgot');
+    Route::post('/password/forgot', [AuthController::class, 'sendResetLink'])->name('password.email');
+
+    Route::get('/password/reset/{token}/{email}', [AuthController::class, 'resetPassword'])->name('password.reset');
+    Route::post('/password/reset', [AuthController::class, 'updatePassword'])->name('password.update');
 });
