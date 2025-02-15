048c4cd110205f84c3f0d6a474b9e6e23c2687bc
minhttran <trcongminh.261002@gmail.com>
2025-02-15 10:10:10 +0700

    feat: implement customer management with CRUD operations and routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Permission\PermissionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Role\RoleController;
 use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Customer\CustomerController;
 
 Route::middleware(['auth:web'])->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
